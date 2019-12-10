00fe9db4c0c862c6698b99578afecb01be4c60ba
J. Wesley Harms <traxtar3@gmail.com>
2019-12-09 20:07:02 -0800

    Update get_passesPrint.py

--- get_passesPrint.py
+++ get_passesPrint.py
@@ -3,7 +3,6 @@ from datetime import datetime, timedelta, date
 import time
 from numpy import fabs
 import re
-from sites import BLE, CAV, CLR, COD, EGL, FYL, THL
 from getTLE import getTLE
 
 # SITE = [lat(N), long(E), alt(km), horizon(deg), begin coverage (closest to zero), end coverage, begin coverage, end coverage(closest to 360)]
