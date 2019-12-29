e6f1db4fb4d9c02794c2aca239c403606638ceb0
J. Wesley Harms <traxtar3@gmail.com>
2019-12-29 02:52:27 -0800

    Update get_TLE.py

--- get_TLE.py
+++ get_TLE.py
@@ -3,10 +3,13 @@ import re
 
 def getTLE(satno, filename):
     ln1 = str("1 " + str(satno))  # Sets up search query for TLE finder
+    while len(ln1) < 7:
+        ln1 = re.sub(' ', '  ', ln1, 1)
     ln2 = str("2 " + str(satno))
+    while len(ln2) < 7:
+        ln2 = re.sub(' ', '  ', ln2, 1)
     ln1 = re.sub(' +', ' ', ln1)
-    L1 = ''
-    L2 = L1
+    L1, L2 = '', ''
     searchfile = open(filename, "r")  # Opens TLE file for TLE that matches satno
     for line in searchfile:
         if ln1 in line:
